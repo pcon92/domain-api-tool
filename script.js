df15894712208f3ec83ad685d075a6c1c551a0d2
Paul C <paul_congiu@hotmail.com>
2021-04-26 17:01:48 +1000

    basic parameter search with basic CSS

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+const PROPERTY_TILE = document.getElementById("property-tile-div");
+const BEDROOMS_MIN = document.getElementById("bedrooms-min-select");
+const BEDROOMS_MAX = document.getElementById("bedrooms-max-select");
+const BATHROOMS_MIN = document.getElementById("bathrooms-min-select");
+const BATHROOMS_MAX = document.getElementById("bathrooms-max-select");
+const CARSPACES_MIN = document.getElementById("carspaces-min-select");
+const CARSPACES_MAX = document.getElementById("carspaces-max-select");
+const NUMBER_OF_PROPERTIES = document.getElementById("number-of-properties-select");
 
-const PROPERTY_ADDRESS = document.getElementById("property-address");
-const PROPERTY_IMAGE = document.getElementById("property-image");
-const PROPERTY_BEDROOMS = document.getElementById("property-bedrooms");
-const PROPERTY_BATHROOMS = document.getElementById("property-bathrooms");
-const PROPERTY_CAR_SPACES = document.getElementById("property-car-spaces");
-const PROPERTY_PRICE = document.getElementById("price");
-const PROPERTY_DESCRIPTION = document.getElementById("description-div")
+let id_array = [];
+const auth_url = 'https://api.domain.com.au/v1/listings/residential/_search'; 
 
-const property_id = '2016886197';
 
-const id_url = `https://api.domain.com.au/v1/listings/${property_id}`
+document.addEventListener("submit", e => {
 
+    e.preventDefault(); 
+
+    const  formValueArray = {
+        "minBedrooms" : BEDROOMS_MIN.value,
+        "maxBedrooms" : BEDROOMS_MAX.value,
+        "minBathrooms" : BATHROOMS_MIN.value,
+        "maxBathrooms" :BATHROOMS_MAX.value,
+        "minCarspaces": CARSPACES_MIN.value,
+        "maxCarSpaces" : CARSPACES_MAX.value,
+        "pageSize" : NUMBER_OF_PROPERTIES.value,
+        "pageNumber" : Math.floor(Math.random() * 10)
+    }
+    
+    get_property_info(formValueArray);
+
+});
 
 async function get_property_from_id() {
+
+    const property_id = '2016886197';
+    const id_url = `https://api.domain.com.au/v1/listings/${property_id}`
 
     const result = await fetch(id_url, {
         method: 'GET',
@@ -22,28 +43,17 @@ async function get_property_from_id() {
         }
     })
 
-    const result_json = await result.json();
-    console.log(result_json);
+    id_array = [];
+    id_array.push(property_id);
+    get_property_from_id_array(id_array);
 
-    PROPERTY_ADDRESS.innerText = result_json.addressParts.displayAddress;
-
-    const propImageDiv = document.createElement("img");
-    propImageDiv.setAttribute("src", `${result_json.media[0].url}`);
-    propImageDiv.setAttribute("height", "300");
-    propImageDiv.setAttribute("width", "300");
-    propImageDiv.setAttribute("alt", "property-image");
-    PROPERTY_IMAGE.appendChild(propImageDiv);
-
-    PROPERTY_BEDROOMS.innerText = `Bedrooms: ${result_json.bedrooms}`;
-    PROPERTY_BATHROOMS.innerText = `Bathrooms: ${result_json.bathrooms}`;
-    PROPERTY_CAR_SPACES.innerText = `Car Spaces: ${result_json.carspaces}`;
-    PROPERTY_PRICE.innerText = `Price: ${result_json.priceDetails.displayPrice}`;
-    PROPERTY_DESCRIPTION.innerText = `${result_json.description}`;
 }
 
-const auth_url = 'https://api.domain.com.au/v1/listings/residential/_search'; 
 
-async function get_property_info() {
+async function get_property_info(formValueArray) {
+    
+    id_array = [];
+
     const data = await fetch(auth_url, {
         method: 'POST',
         headers: {
@@ -51,22 +61,86 @@ async function get_property_info() {
         'Content-Type': 'application/json',
         'X-Api-Key' : API_KEY
         },
-        body: JSON.stringify({
-                "minBedrooms": 5, 
-                "maxBedrooms": 6,
-          })
+        body: JSON.stringify(formValueArray)
     });
 
     const data_json = await data.json();
 
-    console.log(data_json);
-    console.log(data_json[0].listings[0].id);
+    for (el in data_json) {
+        if (data_json[el].type === "Project") {
+            id_array.push(data_json[el].listings[0].id.toString())
+        } else {
+            id_array.push(data_json[el].listing.id.toString())
+        }
+
+    }
+
+    get_property_from_id_array(id_array);
 
 }
 
 
-/* Search Parameters */ 
+async function get_property_from_id_array(id_array) {
+
+    for (id in id_array) {
+
+        const id_url = `https://api.domain.com.au/v1/listings/${id_array[id]}`
+
+        const result = await fetch(id_url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'X-Api-Key' : API_KEY
+            }
+        })
+
+
+        const result_json = await result.json();
+
+        const newPropTileDiv = document.createElement("div");
+        newPropTileDiv.setAttribute("id", `${id}`);
+        newPropTileDiv.setAttribute("class", "property-tile");
+        PROPERTY_TILE.appendChild(newPropTileDiv);
+        
+        const propImageDiv = document.createElement("img");
+        propImageDiv.setAttribute("src", `${result_json.media[0].url}`);
+        propImageDiv.setAttribute("height", "100");
+        propImageDiv.setAttribute("width", "100");
+        propImageDiv.setAttribute("alt", "property-image");
+        newPropTileDiv.appendChild(propImageDiv);
+
+        const newPropTileDivAddress = document.createElement("div");
+        newPropTileDivAddress.innerText = result_json.addressParts.displayAddress;
+        newPropTileDiv.appendChild(newPropTileDivAddress);
+
+        const newPropTileDivBedrooms = document.createElement("div");
+        newPropTileDivBedrooms.innerText = `Bedrooms: ${result_json.bedrooms}`;
+        newPropTileDiv.appendChild(newPropTileDivBedrooms);
+
+        const newPropTileDivBathrooms = document.createElement("div");
+        newPropTileDivBathrooms.innerText = `Bathrooms: ${result_json.bathrooms}`;
+        newPropTileDiv.appendChild(newPropTileDivBathrooms);
+
+        const newPropTileDivCarspaces = document.createElement("div");
+        newPropTileDivCarspaces.innerText = `Car Spaces: ${result_json.carspaces}`;
+        newPropTileDiv.appendChild(newPropTileDivCarspaces);
+
+        const newPropTileDivPrice = document.createElement("div");
+        newPropTileDivPrice.innerText = `Price: ${result_json.priceDetails.displayPrice}`;
+        newPropTileDiv.appendChild(newPropTileDivPrice);
+
+        const newPropTileDivDescription = document.createElement("div");
+        newPropTileDivDescription.innerText = `${result_json.description}`;
+        newPropTileDivDescription.setAttribute("class", "description-div");
+        newPropTileDiv.appendChild(newPropTileDivDescription);
+
+    }   
+
+}
+
 
 /*
+
+
 
 */
